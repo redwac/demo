dad7b9281057f54ac9cc7fa1df5ec39b228f07b3
redwac <sabbane.reda@gmail.com>
2021-05-01 22:29:45 +0000

    Creation classe student et faire l'appler depuit la classe main qui contien maintenent le controller et le get mapping de Student

---
+++ demo/src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,88 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+
+public class Student {
+    private Long id;
+    private String Name;
+    private String email;
+    private LocalDate dob;
+    private int age;
+
+    public Student() {
+
+    }
+
+    public Student(Long id,
+                   String name,
+                   String email,
+                   LocalDate dob,
+                   int age) {
+        this.id = id;
+        Name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+    }
+
+    public Student(String name,
+                   String email,
+                   LocalDate dob,
+                   int age) {
+        Name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", Name='" + Name + '\'' +
+                ", email='" + email + '\'' +
+                ", dob=" + dob +
+                ", age=" + age +
+                '}';
+    }
+}
